0073358f81717ef51bbff60ffe874172c419c2f1
Samuel Abels <knipknap@gmail.com>
2018-11-11 19:01:27 +0100

    clean the query parser up

--- django_find/parsers/query.py
+++ django_find/parsers/query.py
@@ -3,18 +3,16 @@ import re
 from .parser import Parser
 from ..dom import Group, And, Or, Not, Term
 
-tokens = [('and',          re.compile(r'and\b', re.I)),
-          ('or',           re.compile(r'or\b',  re.I)),
-          ('not',          re.compile(r'not\b', re.I)),
-          ('openbracket',  re.compile(r'\(')),
+tokens = [('and', re.compile(r'and\b', re.I)),
+          ('or', re.compile(r'or\b', re.I)),
+          ('not', re.compile(r'not\b', re.I)),
+          ('openbracket', re.compile(r'\(')),
           ('closebracket', re.compile(r'\)')),
-          ('whitespace',   re.compile(r'\s+')),
-          ('field',        re.compile(r'([\w\-]+):')),
-          ('word',         re.compile(r'"([^"]*)"')),
-          ('word',         re.compile(r'([^"\s\\\'\)]+)')),
-          ('unknown',      re.compile(r'.'))]
-
-boolean = {'and': And, 'or': Or, 'not': Not}
+          ('whitespace', re.compile(r'\s+')),
+          ('field', re.compile(r'([\w\-]+):')),
+          ('word', re.compile(r'"([^"]*)"')),
+          ('word', re.compile(r'([^"\s\\\'\)]+)')),
+          ('unknown', re.compile(r'.'))]
 
 def open_scope(scopes, scope):
     scopes.append(scopes[-1].add(scope))
@@ -36,6 +34,63 @@ class QueryParser(Parser):
                 raise AttributeError('constructor argument "default" contains'\
                     +' "{}", which is not also in "fields"'.format(name))
 
+    def parse_word(self, scopes, match):
+        child = Or()
+        for name in self.default:
+            name = self.fields[name]
+            child.add(Term.from_query_value(name, match.group(1)))
+        scopes[-1].add(child)
+        close_scope(scopes)
+
+    def parse_field(self, scopes, match):
+        field_name = match.group(1).lower()
+        field = self.fields.get(field_name)
+        if field is None:
+            self.parse_word(scopes, match)
+            return
+
+        # A field value is required.
+        token, match = self._get_next_token()
+        try:
+            value = [match.group(1)]
+        except IndexError:
+            return
+        scopes[-1].add(Term.from_query_value(field, " ".join(value)))
+        close_scope(scopes)
+
+    def parse_boolean(self, scopes, dom_cls, match):
+        try:
+            last_term = scopes[-1].pop()
+        except IndexError:
+            pass
+        else:
+            open_scope(scopes, dom_cls(last_term))
+
+    def parse_and(self, scopes, match):
+        self.parse_boolean(scopes, And, match)
+
+    def parse_or(self, scopes, match):
+        self.parse_boolean(scopes, Or, match)
+
+    def parse_not(self, scopes, match):
+        open_scope(scopes, Not())
+
+    def parse_openbracket(self, scopes, match):
+        open_scope(scopes, Group())
+
+    def parse_closebracket(self, scopes, match):
+        # Leave the current group.
+        while type(scopes[-1]) != type(Group) and not scopes[-1].is_root:
+            scopes.pop()
+        if not scopes[-1].is_root:
+            scopes.pop()
+
+        # Auto-leave the parent scope (if necessary).
+        close_scope(scopes)
+
+    def parse_whitespace(self, scopes, match):
+        pass
+
     def parse(self, query):
         self._reset()
         self.input = query.strip()
@@ -44,82 +99,8 @@ class QueryParser(Parser):
         token, match = self._get_next_token()
 
         while token != 'EOF':
-            if token == 'word':
-                child = Or()
-                for name in self.default:
-                    name = self.fields[name]
-                    child.add(Term.from_query_value(name, match.group(1)))
-                scopes[-1].add(child)
-                close_scope(scopes)
-                token, match = self._get_next_token()
-                continue
-
-            elif token == 'field':
-                # If the given field does not exists, treat it like a
-                # search term.
-                field_name = match.group(1).lower()
-                field = self.fields.get(field_name)
-                if field is None:
-                    token = 'word'
-                    continue
-
-                # A field value is required.
-                token, match = self._get_next_token()
-                try:
-                    value = [match.group(1)]
-                except IndexError:
-                    token, match = self._get_next_token()
-                    continue
-
-                scopes[-1].add(Term.from_query_value(field, " ".join(value)))
-                close_scope(scopes)
-
-                token, match = self._get_next_token()
-                continue
-
-            elif token == 'and' or token == 'or':
-                try:
-                    last_term = scopes[-1].pop()
-                except IndexError:
-                    pass
-                else:
-                    dom_cls = boolean[token]
-                    open_scope(scopes, dom_cls(last_term))
-                token, match = self._get_next_token()
-                continue
-
-            elif token == 'not':
-                open_scope(scopes, Not())
-                token, match = self._get_next_token()
-                continue
-
-            elif token == 'openbracket':
-                open_scope(scopes, Group())
-                token, match = self._get_next_token()
-                continue
-
-            elif token == 'closebracket':
-                # Leave the current group.
-                while type(scopes[-1]) != type(Group) and not scopes[-1].is_root:
-                    scopes.pop()
-                if not scopes[-1].is_root:
-                    scopes.pop()
-
-                # Auto-leave the parent scope (if necessary).
-                close_scope(scopes)
-                token, match = self._get_next_token()
-                continue
-
-            elif token == 'whitespace':
-                token, match = self._get_next_token()
-                continue
-
-            elif token == 'EOF':
-                break
-
-            else:
-                #raise Exception('BUG: Unknown token ' + str(token))
-                token, match = self._get_next_token()
-                continue
+            parse_func = getattr(self, 'parse_'+token)
+            parse_func(scopes, match)
+            token, match = self._get_next_token()
 
         return result.optimize()
