e8fec94990f258bae6f4b333f61481a2ae4c1bf6
Samuel Abels <knipknap@gmail.com>
2018-11-23 21:43:32 +0100

    fix: query selector order broke tests

--- tests/serializers/test_django.py
+++ tests/serializers/test_django.py
@@ -1,4 +1,6 @@
 from __future__ import absolute_import, print_function
+from datetime import date, datetime
+import datetime
 from django.test import TestCase
 from django_find.parsers.json import JSONParser
 from django_find.serializers.django import DjangoSerializer
@@ -11,6 +13,17 @@ expected_query2 = """(AND: ('book__chapter__title__icontains', 'foo'))"""
 
 query3 = 'test and updated:"2018-02-01" or updated:^2018-02-02$ added:"^2018-01-01" added:2018-01-02$'
 expected_query3 = """(AND: (OR: (AND: (OR: ('hostname__icontains', 'test'), ('address__icontains', 'test'), ('model__icontains', 'test'), (AND: ), (AND: ), ('hostname__icontains', 'test')), ('updated__year', 2018), ('updated__day', 1), ('updated__month', 2)), (AND: ('updated__year', 2018), ('updated__day', 2), ('updated__month', 2)), ('added__gte', datetime.date(2018, 1, 1)), ('added__lte', datetime.date(2018, 1, 2))))"""
+
+def to_list_recursive(tpl):
+    if not isinstance(tpl, tuple):
+        return tpl
+    return [to_list_recursive(t) for t in sorted(tpl, key=str)]
+
+def prep_result(result_str):
+    result_str = result_str.replace('AND:', "'AND',")
+    result_str = result_str.replace('OR:', "'OR',")
+    tuple_in = eval(result_str)
+    return to_list_recursive(tuple_in)
 
 class DjangoSerializerTest(TestCase):
     def setUp(self):
@@ -28,4 +41,6 @@ class DjangoSerializerTest(TestCase):
         self.assertEqual(str(query), expected_query2)
 
         query = DummyModel.q_from_query(query3)
-        self.assertEqual(str(query), expected_query3)
+        query = prep_result(str(query))
+        expected = prep_result(expected_query3)
+        self.assertListEqual(query, expected)
