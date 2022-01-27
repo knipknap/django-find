ddc23ac08703e2b212126adc5dc98ff3e0bdd58e
Robin Sinner <62691050+rsinner99@users.noreply.github.com>
2022-01-27 10:33:05 +0100

    Update setup.py to allow newer Django versions

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     author='Samuel Abels',
     author_email='knipknap@gmail.com',
     install_requires=['future',
-                      'Django>=1.11,<2',
+                      'Django>=1.11,<=3.2',
                       'mysqlclient',
                       'dateparser'],
     keywords=' '.join(['django',
